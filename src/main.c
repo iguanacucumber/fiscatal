101383260118ebb44d10e264121a009253f26e31
iguanacucumber <you@example.com>
2025-02-22 22:25:05 +0400

    add formatters settings

--- src/main.c
+++ src/main.c
@@ -2,26 +2,25 @@
 #include <SDL3_ttf/SDL_ttf.h>
 
 int main(void) {
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window *window = NULL;
-  SDL_Renderer *renderer = NULL;
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
 
-  SDL_CreateWindowAndRenderer("FiscAtal", 800, 600, 0, &window, &renderer);
-  // SDL_SetWindowFullscreen(window, true);
+	SDL_CreateWindowAndRenderer("FiscAtal", 800, 600, 0, &window, &renderer);
+	// SDL_SetWindowFullscreen(window, true);
 
-  bool quit = false;
-  while (!quit) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_EVENT_QUIT)
-        quit = true;
-    }
+	bool quit = false;
+	while (!quit) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) quit = true;
+		}
 
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderPresent(renderer);
-  }
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderPresent(renderer);
+	}
 
-  SDL_Quit();
-  return 0;
+	SDL_Quit();
+	return 0;
 }
